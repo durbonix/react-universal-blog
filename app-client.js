b156696bfbf9e1fc0343acf4b0e11cdf8a88d668
Robert Durbin <robert.durbin@gmail.com>
2017-09-24 21:35:55 -0400

    added babelrc

--- app-client.js
+++ app-client.js
@@ -1,19 +1,29 @@
 // app-client.js
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
-const history = createBrowserHistory();
+import { BrowserRouter, Route } from 'react-router-dom';
 
-// Routes
-//import routes from './routes';
-//console.log(routes);
+const PrimaryLayout = () => (
+  <div className="primary-layout">
+    <header>
+      Our React Router 4 App
+    </header>
+    <main>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/users" component={UsersPage} />
+    </main>
+  </div>
+)
 
-const Routes = () => (
-    <Router>
-      <div>hi</div>
-    </Router>
-);
+const HomePage =() => <div>Home Page</div>
+const UsersPage = () => <div>Users Page</div>
+
+
+const App = () => (
+  <BrowserRouter>
+    <PrimaryLayout />
+  </BrowserRouter>
+)
 
 const app = document.getElementById('app');
-//render(Routes, app);
+render(<App />, app);
